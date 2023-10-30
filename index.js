022059d617f59fefab9ccb01e9894ba1251b0b62
Leonid <l.boychuk@yellowit.ru>
2023-10-30 23:03:02 +1100

    Теттовая правка

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const TelegramApi = require('node-telegram-bot-api')
-// const {gameOptions, againOptions} = require('./options')
-// const sequelize = require('./db');
-// const UserModel = require('./models');
+const TelegramBot = require('node-telegram-bot-api');
+const OpenAI = require('openai');
+const token = '6436291008:AAF6CEgLr2GbnxGS2SOUs_Np7OoXSpxo2RI';
+const openai = new OpenAI({ apiKey: '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' });
 
-const token = '6436291008:AAF6CEgLr2GbnxGS2SOUs_Np7OoXSpxo2RI'
+const bot = new TelegramBot(token, { polling: true });
 
-const bot = new TelegramApi(token, {polling: true})
-
-const chats = {}
-
-
-bot.on('message', msg =>{
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
     const text = msg.text;
-    const chatID = msg.chat.id;
 
-    if (text.includes('Аня')) {
-        bot.sendMessage(chatID, 'Сообщение содержит слово Аня');
-    } else {
-        bot.sendMessage(chatID, 'Сообщение не содержит слово Аня');
-    }
+    openai.createCompletion({
+        engine: 'text-davinci-003', // Используйте подходящую версию GPT-3
+        prompt: text,
+        max_tokens: 50, // Максимальное количество токенов в ответе
+    })
+        .then((response) => {
+            const generatedText = response.choices[0].text;
+            bot.sendMessage(chatId, generatedText);
+        })
+        .catch((error) => {
+            console.error(error);
+            bot.sendMessage(chatId, 'Произошла ошибка при генерации текста.');
+        });
 })
